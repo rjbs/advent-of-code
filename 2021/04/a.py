47f839bbec05a4e4c92872c331662efb6096376f
Ricardo Signes <rjbs@semiotic.systems>
2021-12-04 17:18:57 -0500

    day 4, warts and all

---
+++ 2021/04/a.py
@@ -0,0 +1,62 @@
+seq   = []
+
+with open('input.txt') as f:
+    seq = list(map(int,f.readline().rstrip().split(",")))
+
+    f.readline() # line separating cards from sequence
+
+    def card_line(smap):
+        return [ dict(hit=False, value=i) for i in map(int, smap) ]
+
+    cards = [
+        list(map(card_line, map(str.split, hunk.split("\n"))))
+        for hunk in f.read().rstrip().split("\n\n")
+    ]
+
+def card_won(card):
+    for row in card:
+        if all(map(lambda c: c['hit'], row)): return f"row {row}"
+
+    for i in range(5):
+        if all(map(lambda r: r[i]['hit'], list(card))): return f"col {i}"
+
+    # if all(map(lambda i: card[i][i]['hit'], list(range(5)))): return "d1"
+    # if all(map(lambda i: card[i][4-i]['hit'], list(range(5)))): return "d2"
+
+    return None
+
+def print_card(card):
+    for row in card:
+        print(" ".join(
+            [ f"{c['value']:2}x" if c['hit'] else f"{c['value']:2} " for c in row ]
+        ))
+
+def card_value(card):
+    value = 0
+    for row in card:
+        print(f"{row}")
+        for cell in filter(lambda c: not c['hit'], row):
+            print(f"* {cell['value']}")
+            value += cell['value']
+
+    return value
+
+done = False
+for n in seq:
+    if done: break
+
+    print(n)
+    for card in cards:
+        for row in card:
+            for cell in row:
+                if cell['value'] == n: cell['hit'] = True
+
+        how = card_won(card)
+        if (how):
+            print(f"We got a winner! {how}")
+            print_card(card)
+            print(n * card_value(card))
+            done = True
+            break
+        else:
+            print_card(card)
